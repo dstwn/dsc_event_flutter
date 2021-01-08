beede8ed8d8689d236dadd15131778070fd1d84e
Dwi Setiawan <dwisetiawan@linuxmail.org>
2021-01-08 21:32:18 +0700

    add constant class in utils/constant

---
+++ lib/utils/constant.dart
@@ -0,0 +1,7 @@
+import 'dart:core';
+
+class Constant {
+  static const String BASE_URL = "";
+  static const String LOGIN_URL = BASE_URL + "/login";
+  static const String REGISTER_URL = BASE_URL + "/resgiter";
+}
